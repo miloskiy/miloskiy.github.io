dc127b0ce0034cb7954da2a331a319e7ef112851
Sergii Polkovnikov <sergpolkovnikov@gmail.com>
2021-03-05 14:17:40 +0100

    Update page name

--- src/gatsby-theme-carbon/components/Header.js
+++ src/gatsby-theme-carbon/components/Header.js
@@ -3,7 +3,7 @@ import Header from 'gatsby-theme-carbon/src/components/Header';
 
 const CustomHeader = (props) => (
   <Header {...props}>
-    Compass Design System
+    <b>Compass</b> - GetYourGuide Design System
   </Header>
 );
 
